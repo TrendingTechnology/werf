752ab86012f0de0711b0f30db4485aa583419745
Alexey Igrychev <alexey.igrychev@flant.com>
2021-01-25 11:11:57 +0000

    Remove unused code

--- pkg/giterminism_inspector/giterminism_inspector.go
+++ pkg/giterminism_inspector/giterminism_inspector.go
@@ -2,7 +2,6 @@ package giterminism_inspector
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/werf/logboek"
 )
@@ -12,7 +11,6 @@ const giterminismDocPageURL = "https://werf.io/v1.2-alpha/documentation/advanced
 var (
 	LooseGiterminism         bool
 	NonStrict                bool
-	DevMode                  bool
 	ReportedUncommittedPaths []string
 	ReportedUntrackedPaths   []string
 )
@@ -20,47 +18,13 @@ var (
 type InspectionOptions struct {
 	LooseGiterminism bool
 	NonStrict        bool
-	DevMode          bool
 }
 
 func Init(projectPath string, opts InspectionOptions) error {
 	LooseGiterminism = opts.LooseGiterminism
 	NonStrict = opts.NonStrict
-	DevMode = opts.DevMode
 
 	return nil
-}
-
-func ReportUntrackedFile(ctx context.Context, path string) error {
-	for _, p := range ReportedUntrackedPaths {
-		if p == path {
-			return nil
-		}
-	}
-	ReportedUntrackedPaths = append(ReportedUntrackedPaths, path)
-
-	if NonStrict {
-		logboek.Context(ctx).Warn().LogF("WARNING: Untracked file %s was not taken into account (more info %s)\n", path, giterminismDocPageURL)
-		return nil
-	} else {
-		return fmt.Errorf("restricted usage of untracked file %s (more info %s)", path, giterminismDocPageURL)
-	}
-}
-
-func ReportUncommittedFile(ctx context.Context, path string) error {
-	for _, p := range ReportedUncommittedPaths {
-		if p == path {
-			return nil
-		}
-	}
-	ReportedUncommittedPaths = append(ReportedUncommittedPaths, path)
-
-	if NonStrict {
-		logboek.Context(ctx).Warn().LogF("WARNING: Uncommitted file %s was not taken into account (more info %s)\n", path, giterminismDocPageURL)
-		return nil
-	} else {
-		return fmt.Errorf("restricted usage of uncommitted file %s (more info %s)", path, giterminismDocPageURL)
-	}
 }
 
 func PrintInspectionDebrief(ctx context.Context) {
